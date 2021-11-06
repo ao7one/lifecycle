554633116204630529a0474f47efe3e8af9aeebf
ao7one <ao7one@gmail.com>
2021-11-06 12:32:18 +0000

    init

--- security.py
+++ security.py
@@ -1,5 +1,12 @@
 # Get known CVE's with fixes and convert to JSON
 
-import requests
+import pandas
 
 url = "https://www14.software.ibm.com/webapp/set2/flrt/doc?page=aparCSV"
+timeout = 5.0
+
+csv=pandas.read_csv(url)
+print(csv)
+#dict_from_csv=pandas.read_csv(url, header=None, index_col=0).to_dict()
+#print(dict_from_csv)
+
