973a5219eac9250eae996e451133ee933cb8c992
ao7one <ao7one@gmail.com>
2021-11-06 01:02:29 +0000

    init

--- lifecycle.py
+++ lifecycle.py
@@ -1,2 +1,8 @@
 import requests
 
+url = "https://www14.software.ibm.com/webapp/set2/flrt/liteTable?prodKey=aix&format=json"
+timeout = 5
+
+response = requests.get(url, timeout=timeout)
+
+print(response.json())
