962a41fdb12b747f7f77a934c00aa6f15570b46f
ao7one <ao7one@gmail.com>
2021-11-06 10:57:23 +0000

    init

--- lifecycle.py
+++ lifecycle.py
@@ -1,3 +1,5 @@
+# List product lifecycle data
+
 import requests
 
 url = "https://www14.software.ibm.com/webapp/set2/flrt/liteTable?prodKey=aix&format=json"
@@ -6,4 +8,5 @@ timeout = 5
 response = requests.get(url, timeout=timeout)
 payload = response.json()
 
-print(payload["input"])
+for result in payload['results']:
+    print(result['input'], result['ga'], result['eosps'])
