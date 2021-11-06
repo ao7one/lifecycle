c4a0b4772cf9f9b4bffc40c28f60f42a9aeb9930
ao7one <ao7one@gmail.com>
2021-11-06 11:05:18 +0000

    error handle

--- lifecycle.py
+++ lifecycle.py
@@ -1,12 +1,37 @@
 # List product lifecycle data
+#
+# Expected response from FLRT API
+# {"results": [
+#     {
+#         "input": "7200-05-03",
+#         "inputurl": "http://www.ibm.com/support/fix...",
+#         "ga": "2021.09.10",
+#         "eosps": "2023.11.30"
+#     },
+#     {
+#         "input": "7200-05-02",
+#         "inputurl": "http://www.ibm.com/support/fix....",
+#         "ga": "2021.04.16",
+#         "eosps": "2023.11.30"
+#     }
+# ]}
+#
 
 import requests
 
 url = "https://www14.software.ibm.com/webapp/set2/flrt/liteTable?prodKey=aix&format=json"
 timeout = 5
 
-response = requests.get(url, timeout=timeout)
-payload = response.json()
+try:
+    response = requests.get(url, timeout=timeout)
+    response.raise_for_status()
 
-for result in payload['results']:
-    print(result['input'], result['ga'], result['eosps'])
+    # Load JSON response to dict
+    payload = response.json()
+
+    # Parse output
+    for result in payload['results']:
+        print(result['input'], result['ga'], result['eosps'])
+
+except requests.RequestException as reqEx:
+    print(reqEx)
